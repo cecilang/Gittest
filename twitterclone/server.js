67e2ba95bddee6161c1fad9cf7772cd0f4d83984
Cecil Ang <cecil@betafish.co>
2016-10-14 14:57:54 -0700

    hooked up spouts to db

--- twitterclone/server.js
+++ twitterclone/server.js
@@ -1,24 +1,39 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
 var bodyParser = require('body-parser');
 
 var app = express(); //initialize express
+var db = null;
+
+//connect to mongodb
+MongoClient.connect("mongodb://localhost:27017/spouts", function(err, dbconnect) {
+  if(!err) {
+    console.log("connected to mongodb");
+    db = dbconnect
+  }
+});
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-var spouts = [
-  'I just saw a shark!',
-  'A fishing boat just came by... ugh',
-  'Wowed some simpleton humans with my spouts.',
-];
-
 app.get('/spouts', function(req, res, next) {  //do this when someone hits the url
-  res.send(spouts);
+  db.collection('spouts', function(err, spoutsCollection) {
+    spoutsCollection.find().toArray(function(err, spouts){ //look up array of spouts in db
+      return res.json(spouts);
+    });
+  });
 });
 
 app.post('/spouts', function(req, res, next) {
-  console.log(req.body); //require body parser for json data being posted
-  res.send();
+  db.collection('spouts', function(err, spoutsCollection) {
+    var newSpout = {
+      text: req.body.newSpout
+    };
+
+    spoutsCollection.insert(newSpout, {w:1}, function(err){ //insert new spouts with object, mongo's write option, and callback
+      return res.send(); //just to say it works, without an object
+    });
+  });
 });
 
 app.listen(3000, function(){
