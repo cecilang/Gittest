b62d2e8ebdd43063b8a9bf86649bc719c51b9196
Cecil Ang <cecil@betafish.co>
2016-10-15 03:32:16 -0700

    encrpyt password, enable sessions and prevent unauthorized activities

--- twitterclone/server.js
+++ twitterclone/server.js
@@ -3,12 +3,14 @@ var MongoClient = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 var bodyParser = require('body-parser');
 var bcrypt = require('bcryptjs');
+var jwt = require('jwt-simple');
 
 var app = express(); //initialize express
 var db = null;
+var JWT_SECRET = 'fishspouts';
 
 //connect to mongodb
-MongoClient.connect("mongodb://localhost:27017/spouts", function(err, dbconnect) {
+MongoClient.connect("mongodb://localhost:27017/spoutsdb", function(err, dbconnect) {
   if(!err) {
     console.log("connected to mongodb");
     db = dbconnect
@@ -28,9 +30,15 @@ app.get('/spouts', function(req, res, next) {  //do this when someone hits the u
 
 //create a new spout on server
 app.post('/spouts', function(req, res, next) {
+
+  var token = req.headers.authorization;
+  var user = jwt.decode(token, JWT_SECRET);
+
   db.collection('spouts', function(err, spoutsCollection) {
     var newSpout = {
-      text: req.body.newSpout
+      text: req.body.newSpout,
+      user: user._id,
+      username: user.username,
     };
 
     spoutsCollection.insert(newSpout, {w:1}, function(err){ //insert new spouts with object, mongo's write option, and callback
@@ -41,10 +49,14 @@ app.post('/spouts', function(req, res, next) {
 
 //remove a spout on server
 app.put('/spouts/remove', function(req, res, next) {
+
+  var token = req.headers.authorization;
+  var user = jwt.decode(token, JWT_SECRET);
+
   db.collection('spouts', function(err, spoutsCollection) {
     var spoutId = req.body.spout._id; //parse entire spout back and get by id
 
-    spoutsCollection.remove({_id: ObjectID(spoutId)}, {w:1}, function(err){ //insert new spouts with mongo ObjectID, mongo's write option, and callback
+    spoutsCollection.remove({_id: ObjectID(spoutId), user: user._id}, {w:1}, function(err){ //insert new spouts with mongo ObjectID, mongo's write option, and callback
       return res.send(); //just to say it works, without an object
     });
   });
@@ -58,12 +70,11 @@ app.post('/users', function(req, res, next) {
         bcrypt.hash(req.body.password, salt, function(err, hash) {
           //hash replaced password
           var newUser = {
-            email: req.body.email,
+            username: req.body.username,
             password: hash, //store hash instead of password
           };
 
           usersCollection.insert(newUser, {w:1}, function(err){ //insert new user var, mongo's write option, and callback
-            return res.send(); //just to say it works, without an object
           });  // Store hash in your password DB.
         });
     });
@@ -74,12 +85,13 @@ app.post('/users', function(req, res, next) {
 app.put('/users/signin', function(req, res, next) {
   db.collection('users', function(err, usersCollection) {
 
-    usersCollection.findOne({email: req.body.email}, function(err, user){ //find just one user
+    usersCollection.findOne({username: req.body.username}, function(err, user){ //find just one user
 
       //compare password entered to DB
       bcrypt.compare(req.body.password, user.password, function(err, result){
         if (result){
-          return res.send();
+          var token = jwt.encode(user, JWT_SECRET); //injecting token for cookie session
+          return res.json({token: token}); //brackets for res.json
         } else{
           return res.status(400).send();
         }
