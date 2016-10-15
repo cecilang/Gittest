c832ddf518251fa259d6ea64e099210ad05fad2e
Cecil Ang <cecil@betafish.co>
2016-10-14 19:47:35 -0700

    created signup and signin processes

--- twitterclone/server.js
+++ twitterclone/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
 var bodyParser = require('body-parser');
+var bcrypt = require('bcryptjs');
 
 var app = express(); //initialize express
 var db = null;
@@ -24,6 +26,7 @@ app.get('/spouts', function(req, res, next) {  //do this when someone hits the u
   });
 });
 
+//create a new spout on server
 app.post('/spouts', function(req, res, next) {
   db.collection('spouts', function(err, spoutsCollection) {
     var newSpout = {
@@ -32,6 +35,55 @@ app.post('/spouts', function(req, res, next) {
 
     spoutsCollection.insert(newSpout, {w:1}, function(err){ //insert new spouts with object, mongo's write option, and callback
       return res.send(); //just to say it works, without an object
+    });
+  });
+});
+
+//remove a spout on server
+app.put('/spouts/remove', function(req, res, next) {
+  db.collection('spouts', function(err, spoutsCollection) {
+    var spoutId = req.body.spout._id; //parse entire spout back and get by id
+
+    spoutsCollection.remove({_id: ObjectID(spoutId)}, {w:1}, function(err){ //insert new spouts with mongo ObjectID, mongo's write option, and callback
+      return res.send(); //just to say it works, without an object
+    });
+  });
+});
+
+//create a new user on server
+app.post('/users', function(req, res, next) {
+  db.collection('users', function(err, usersCollection) {
+
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(req.body.password, salt, function(err, hash) {
+          //hash replaced password
+          var newUser = {
+            email: req.body.email,
+            password: hash, //store hash instead of password
+          };
+
+          usersCollection.insert(newUser, {w:1}, function(err){ //insert new user var, mongo's write option, and callback
+            return res.send(); //just to say it works, without an object
+          });  // Store hash in your password DB.
+        });
+    });
+  });
+});
+
+//sign in user on server
+app.put('/users/signin', function(req, res, next) {
+  db.collection('users', function(err, usersCollection) {
+
+    usersCollection.findOne({email: req.body.email}, function(err, user){ //find just one user
+
+      //compare password entered to DB
+      bcrypt.compare(req.body.password, user.password, function(err, result){
+        if (result){
+          return res.send();
+        } else{
+          return res.status(400).send();
+        }
+      });
     });
   });
 });
